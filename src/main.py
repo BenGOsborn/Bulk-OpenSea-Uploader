01a7f90396228400193480572a0549e9867e7537
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-11-20 15:27:23 +1100

    🎉 Update

--- src/main.py
+++ src/main.py
@@ -4,6 +4,21 @@ class Uploader:
     def __init__(self):
         self.__driver = webdriver.Chrome()
 
+    def connect(self):
+        '''
+        Connect to wallet. https://dev.to/ltmenezes/automated-dapps-scrapping-with-selenium-and-metamask-2ae9
+        '''
+        pass
+
+    def __upload(self):
+        '''
+        Upload a single NFT to OpenSea.
+        '''
+        pass
+
+    def close(self):
+        self.__driver.close()
+
 def main():
     pass
 
